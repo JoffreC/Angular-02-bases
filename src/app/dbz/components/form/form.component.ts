983e28cd9e4715005b277fac4c53dcbeaea243ca
JoffreC <joffre.condor@epn.edu.ec>
2024-04-13 14:48:50 -0500

    Seccion 5 1

---
+++ src/app/dbz/components/form/form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'add-character',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+
+}
---
+++ src/app/dbz/interfaces/character.interface.ts
@@ -0,0 +1,5 @@
+export interface Character
+{
+  nombre: string;
+  poder: number;
+}
